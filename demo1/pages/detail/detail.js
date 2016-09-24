f88da86614d4717749b9ad79a4cb0d883ce2d371
Andrew <andrewsy@163.com>
2016-09-24 14:41:56 +0800

    update the demo code.

--- demo1/pages/detail/detail.js
+++ demo1/pages/detail/detail.js
@@ -1,8 +1,18 @@
+var userservice = require("../../services/userservice.js")
+
 Page({
 	data: {
-		title: "abc",
-		content: "something in here",
-		visitor: "Andrew",
-		date: "2016/09/24"
+		title: "Log Title",
+		content: "There is logs content in here",
+		visitor: "Author",
+		date: "0000/00/00"
+	},
+	onLoad: function () {
+		var _this = this;
+		userservice.getUserInfo(function (userInfo){
+			_this.setData({
+				visitor: userInfo.nickName
+			});
+		});
 	}
 });
